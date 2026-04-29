ebda3af12ef76418573c77c6bcb59048b5c7b0a6
Suji Min <heysaysuji@icloud.com>
2026-04-30 07:09:02 +1000

    Add files via upload
    This is the program code that answers the addition question. If you enter the correct calculation result, you will get the phrase "genius" and if you enter the wrong value, you will get the phrase "absurd."

---
+++ addition_quiz_program.py
@@ -0,0 +1,12 @@
+# a program that answers an additional question
+
+
+x = input("12 + 23 = ") # show the question and get answers, and save in x(String)
+a = int(x) # change String X to int X so that we can compare
+
+
+if a == 12+23:
+    print("You are a genius!")
+
+else:
+    print("Stupid!")
