386d322fb1791f85ba59c0388d6d6c5aae3511d6
Suji Min <heysaysuji@icloud.com>
2026-07-06 14:36:01 +1000

    Add files via upload
    I created a program to draw using a mouse.

---
+++ draw_by_controlling_a_turtle_with_mouse.py
@@ -0,0 +1,7 @@
+import turtle as t
+
+t.speed(0)
+t.pensize(2)
+t.hideturtle() #hide turtle from the window
+t.onscreenclick(t.goto) #turtle moves and draw the line while moving to the speficif location
+
